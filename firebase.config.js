560ba945b2b9a3c6e3db720ab2c1271208ab743f
YSL0701 <30719leo@gmail.com>
2018-11-05 00:19:40 +0800

    firebase config

---
+++ firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyDm8iP_tyFekYJazLa3CHMIWopHqlzs-JE',
+  authDomain: 'sweetaste-dessert.firebaseapp.com',
+  databaseURL: 'https://sweetaste-dessert.firebaseio.com',
+  projectId: 'sweetaste-dessert',
+  storageBucket: 'sweetaste-dessert.appspot.com',
+  messagingSenderId: '820812977963'
+}
