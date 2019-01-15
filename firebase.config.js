3e3a1bb1a4e3fc07d28021a0d3388a21c913643c
YSL0701 <30719leo@gmail.com>
2019-01-16 02:43:46 +0800

    firebase config untrack

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyDm8iP_tyFekYJazLa3CHMIWopHqlzs-JE',
-  authDomain: 'sweetaste-dessert.firebaseapp.com',
-  databaseURL: 'https://sweetaste-dessert.firebaseio.com',
-  projectId: 'sweetaste-dessert',
-  storageBucket: 'sweetaste-dessert.appspot.com',
-  messagingSenderId: '820812977963'
-}
